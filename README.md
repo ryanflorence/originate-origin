462837cacf58daa88dad4f79856dff385c43081b
Ryan Florence <rpflorence@gmail.com>
2013-12-01 22:17:01 -0700

    first

---
+++ README.md
@@ -0,0 +1,35 @@
+originate-origin
+================
+
+[loom][1] origin for creating loom origins (yo dawg).
+
+Usage
+-----
+
+Lets pretend you wrote the most stylish framework in world named
+'tophat'. Now you want to create an application development
+project "scaffold" for your users:
+
+```
+$ npm install -g loom
+# > yo dawg, I heard you like originating origins with originate
+$ originate origin originate-tophat
+# > so I put an origin in your originate
+$ cd originate-tophat
+# > so you can originate while you origin
+# add some files to loom/templates/
+# add some logic to loom/generators/tophat
+$ npm publish
+```
+
+That's it, now people can use it with `originate tophat my-app`.
+
+Licences and Copyright
+----------------------
+
+MIT Style licence
+
+(c) 2013 Ryan Florence
+
+  [1]:https://github.com/rpflorence/loom
+
---
+++ loom/generators/origin.js
@@ -0,0 +1,33 @@
+var glob = require('glob');
+var color = require('cli-color');
+var red = color.red;
+
+exports.before = function(next, env) {
+  env.appName = env.args[0];
+  if (!env.appName.match(/^originate-/)) {
+    console.log(red('\nname invalid "'+env.appName+'"; origins must be named "originate-<name>"\n'));
+    process.exit();
+  }
+  next();
+};
+
+exports.present = function(next, env) {
+  next({
+    appName: env.appName,
+    originName: env.appName.replace(/^originate-/, ''),
+    headerChars: env.appName.split('').map(function() { return '='; }).join('')
+  })
+};
+
+exports.savePath = function(next, env, template) {
+  if (template == 'loom/generators/origin.js.hbs') {
+    template = 'loom/generators/'+env.appName.replace(/^originate-/, '')+'.js.hbs';
+  }
+  next(env.appName+'/'+template.replace('.hbs', ''));
+};
+
+exports.templates = glob.sync(__dirname+'/../templates/**/*.hbs').map(function(template) {
+  var base = __dirname.replace(/generators$/, '')+'templates/';
+  return template.replace(base, '');
+});
+
